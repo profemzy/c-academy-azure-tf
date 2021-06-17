868f6277ba51697958154d95c1b4b4a9bba9a2bb
Oladele Femi <profemzy@gmail.com>
2021-06-17 10:45:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,39 @@ provider "azurerm" {
 data "azurerm_resource_group" "dev" {
   name = "cal-1418-be"
 }
+
+module "network" {
+  source = "cloudacademy/network/azurerm"
+  version = "2.0.1"
+  resource_group_name = "${data.azurerm_resource_group.dev.name}"
+  location = "${data.azurerm_resource_group.dev.location}"
+  subnet_names = ["subnet1"]
+  vnet_name = "web"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+module "security" {
+  source              = "cloudacademy/network-security-group/azurerm//modules/HTTP"
+  resource_group_name = "${data.azurerm_resource_group.dev.name}"
+  location            = "${data.azurerm_resource_group.dev.location}"
+  security_group_name = "web_nsg"
+
+  custom_rules = [
+    {
+      name                   = "ssh"
+      priority               = "200"
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "tcp"
+      destination_port_range = "22"
+      description            = "ssh-access"
+    }
+  ]
+
+  tags = {
+    environment = "dev"
+  }
+}
