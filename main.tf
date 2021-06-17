39c9b0ff71ac4cdb6b290427c8cf64f54ef032ad
Oladele Femi <profemzy@gmail.com>
2021-06-17 10:37:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
   version = "< 2"
-  
+
   # Prevent provider from trying to register Azure Resource Providers
   skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "dev" {
+  name = "cal-1418-be"
 }
